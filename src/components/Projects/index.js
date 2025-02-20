da164b683c5a859f6708e057f8abb7381c887d03
mavish12 <slodh079@gmail.com>
2025-02-21 01:21:00 +0530

    Portfolio done

---
+++ src/components/Projects/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import {
+  Container,
+  Wrapper,
+  Title,
+  Desc,
+  CardContainer,
+  ToggleButtonGroup,
+  ToggleButton,
+  Divider,
+} from "./ProjectsStyle";
+import ProjectCard from "../Cards/ProjectCards";
+import { projects } from "../../data/constants";
+
+const Projects = ({ openModal, setOpenModal }) => {
+  const [toggle, setToggle] = useState("all");
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <CardContainer>
+          {projects.map((project, index) => (
+            <ProjectCard
+              key={index}
+              project={project}
+              openModal={openModal}
+              setOpenModal={setOpenModal}
+            />
+          ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Projects;
---
+++ src/data/constants.js
@@ -0,0 +1,241 @@
+// import DataVizImage from "../images/dataVizimage.png"
+import Mart from "../images/shoppingmart.png"
+import ReportsHub from "../images/reportsHub.png"
+import Moviez from "../images/moviez.png"
+
+export const Bio = {
+  name: "Shivam Lodh",
+  roles: ["Frontend Developer", "Full Stack Developer", "UI/UX Designer"],
+  description:
+    "I am a dynamic and adaptable individual with a strong drive for learning and growth. Always ready to take on new challenges, I am committed to delivering exceptional results. My problem-solving abilities and enthusiasm for innovation allow me to thrive in fast-paced environments. With a positive mindset and a dedication to continuous improvement, I am eager to contribute meaningfully and achieve excellence.",
+  github: "https://github.com/mavish12",
+  linkedin: "https://www.linkedin.com/in/shivam-lodh-b0394224a/",
+  resume:
+    "https://drive.google.com/file/d/1LYZ6w6kM2J9jEBYvQ20D3ri408lDyOSd/view?usp=drive_link",
+};
+
+export const experiences = [
+  {
+    id: 1,
+    img: "https://corp.infogen-labs.com/Infogen-ciklum-white-logo.svg",
+    role: "Frontend Development Intern",
+    company: "Infogen Labs (A Ciklum Company)",
+    date: "Dec 2023 - Aug 2024",
+    desc: "Played a critical role in developing interactive dashboards from scratch, collaborated with backend teams to integrate APIs, implementing React JS,Tailwind CSS, Material UI, Highcharts to enhance user experience and traffic ,also designed and optimized highly responsive web pages using Tailwind CSS, ensuring consistent branding and user experience.",
+    skills: [
+      "JavaScript",
+      "React.js",
+      "Tailwind CSS",
+      "Bootstrap",
+      "Highcharts",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://media.licdn.com/dms/image/v2/D4D0BAQGdlaEjBPju1w/company-logo_200_200/company-logo_200_200/0/1729958373105/trubleagency_logo?e=1747872000&v=beta&t=UiFmG0dIGbj4dujDVg64monCAgJq7l7t3VVtzEyrxLg",
+    role: "Software Development Intern",
+    company: "Truble Services Pvt. Ltd.",
+    date: "Aug 2024 - Dec 2024",
+    desc: "Spearheaded the development of a responsive and user-friendly company website using React JS and Tailwind CSS, ensuring seamless user experience across devices.Collaborated closely with backend developers to instruct for the required API’s for seamless data flow within the website. Implement new features like booking calendar, mailing and call etc. with full responsive user experience.",
+    skills: [
+      "JavaScript",
+      "React.js",
+      "Tailwind CSS",
+      "Figma",
+    ],
+  },
+];
+
+// export const education = [
+//   {
+//     id: 0,
+//     img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/Kiit.jpeg?alt=media&token=3839d520-c59d-4341-ad8f-0980c98de4dd",
+//     collage: "savitribai phule pune university",
+//     date: "Oct 2021 - may 2024",
+//     grade: 8.0,
+//     degree: "Bachelor of Computer Applications",
+//   },
+// ];
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://v4.material-ui.com/static/logo.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Vite js",
+        image:
+          "      https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/vitejs/vitejs-original.svg",
+      },
+      {
+        name: "Redux-Tookit",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "Highcharts",
+        image:
+          "https://cdn.worldvectorlogo.com/logos/highcharts-1.svg",
+      },
+      
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://nodejs.org/static/images/logo.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+  {
+    title: "Development Tools",
+    skills: [
+      {
+        name: "Git",
+        image: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/git/git-plain-wordmark.svg",
+      },
+      {
+        name: "Github",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/githubcodespaces/githubcodespaces-original.svg",
+      },
+      {
+        name: "Chrome Developer Tool",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/chrome/chrome-original.svg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/postman/postman-original.svg",
+      },
+      {
+        name: "Netlify",
+        image:
+          "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/netlify/netlify-original-wordmark.svg",
+      },
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: " Reports Hub",
+    // date: "Sep 2024 - Sep 2024",
+    description:
+      "Developed a Project management web application where teams status and updates can be stores and monitored, and tasks can be assigned where teams can be compared with others and give visual datas. Also test cases can be monitored and data can be stored of passed, failed and skipped text cases with visual graphical representation.",
+    image: ReportsHub,
+    tags: [
+      "React Js",
+      "Redux",
+      "CSS",
+      "Material UI",
+      "Tailwind CSS",
+      "Highcharts",
+      "jsPDF"
+    ],
+    category: "web app",
+    github: "N/A",
+    webapp: "https://www.reportshub.in/",
+  },
+
+  {
+    id: 2,
+    title: "Shopping Mart",
+    // date: "Aug 2024 - Aug 2024",
+    description:
+      "MartShopping is a frontend-only e-commerce website designed for user experience. It focuses on providing a smooth and interactive shopping interface. Users can browse products, view details, filter, sort products according to need and add items to the cart, but no actual transactions or data storage happen. The site is built to showcase UI/UX design and responsiveness, giving a feel of a real shopping platform.",
+    image: Mart,
+    tags: [
+      "Javascript",
+      "React Js",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/mavish12/Shopping-Mart/tree/master/Shopping-Mart",
+    webapp: "https://martshoping.netlify.app/",
+  },
+  // {
+  //   id: 3,
+  //   title: "Meetup Clone",
+  //   // date: "Aug 2024 - Aug 2024",
+  //   description:
+  //     "Developed a web application enabling users to browse, and participate in meetups with seamless navigation.Key features include participant management, and real-time updates. Integrated client-side routing with React Router DOM and built a consistent, maintainable UI using modern JavaScript",
+  //   image: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d9/Meetup_website_screenshot.png/300px-Meetup_website_screenshot.png",
+  //   tags: [
+  //     "React Js",
+  //     "Express Js",
+  //     "MongoDB",
+  //     "Bootstrap",
+  //   ],
+  //   category: "web app",
+  //   github: "https://github.com/checodezz/meetup-frontend",
+  //   webapp: "https://meetup-frontend-tau.vercel.app/",
+  // },
+  {
+    id: 3,
+    title: "Moviez",
+    // date: "Aug 2024 - Aug 2024",
+    description:
+      " Moviez is where you can search for any movie and get details like the title, poster, and description. It’s super simple to use—just type in a movie name, and it fetches the info instantly. I used React to build it, and it pulls data from an external API to keep everything updated. ",
+    image: Moviez,
+    tags: [
+      "React Js",
+      "Bootstrap",
+      "CSS",
+      "Rest Api"
+    ],
+    category: "web app",
+    github: "https://github.com/mavish12/Movie_Name",
+    webapp: "https://getmoviez.netlify.app/",
+  }
+];
+
+export const TimeLineData = [{ year: 2023, text: "Started my journey" }];
